1bc37d5fb3435e4f50fe6d4d6d854572bb24b176
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-07 09:57:20 +0530

    Welcome note

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const server = http.createServer((req,res) => {
-    console.log('Gps')
+    console.log(req.headers, req.method, req.url);
+    res.setHeader('Content-type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>')
+    res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 server.listen(4000)
