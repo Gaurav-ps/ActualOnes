f3f97533769412c5aa46454e819d41c0dce99739
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-10 11:27:29 +0530

    Routers and Filters

---
+++ Express Intro/routes/app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const express = require('express');
+
+const adminRoutes = require('./routes/admin')
+
+const shopRoutes = require('./routes/shop')
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/admin',adminRoutes)
+
+app.use('/shop',shopRoutes)
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
+
+app.listen(3000);
