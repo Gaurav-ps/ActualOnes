e66ae326d58a831702ddf2a303d431b1b4257fe4
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-10 18:55:36 +0530

    Completed basic char project

--- Chat App/app.js
+++ Chat App/app.js
@@ -8,31 +8,40 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',(req,res,next) => {
+    fs.readFile('chats.txt',(err,data) => {
+        if(err){
+            data = 'No chats exists'
+            console.log(err);
+        }
+        res.send(`${data}<form action = "/" method="POST" onSubmit="document.getElementById('username').value = localStorage.getItem('username')">
+        <input id = "message" type="text" name="message">
+        <input type = "hidden" id="username" name="username">
+        <button type="submit">Send</button></form>`)
+    })
+})
+
+app.post('/',(req,res,next) => {
+    console.log(req.body.username);
+    console.log(req.body.message);
+    console.log(`${req.body.username}:${req.body.message}`)
+    fs.writeFile("chats.txt",`${req.body.username}:${req.body.message}`,{flag:'a'},(err) => {
+        if(err) console.log(err);
+        else res.redirect('/')
+    })
+})
 
 app.get('/login',(req, res, next) => {
-    console.log('In the middleware!!')
-    res.send(`<form action = "/" onsubmit = localStorage.setItem("username",document.getElementById("username").value) action="/message" method="POST">
+    res.send(`<form action = "/login" onSubmit = localStorage.setItem("username",document.getElementById("username").value) action="/message" method="POST">
     <input id = "username" type="text" name="username">
     <button type="submit">Login</button></form>`)
 })
 
-app.post('/message',(req, res, next) => {
-    console.log('In the next middleware!!')
-    res.redirect('/')
-})
-
-app.get('/',(req,res,next) => {
-    console.log(req.body);
-    res.send(`<form action = "/" onsubmit = "document.getElementById("username") = localStorage.getItem("username")" method = "POST" >
-    <input id = "message" type="text" name="message">
-    <input id="username" name="username">
-    <button type="submit">Send</button></form>`)
-})
-
-app.post('/',(req,res,next) => {
-    console.log(`${req.username}:${req.message}`)
-    res.redirect('/')
+app.post('/login',(req,res,next) => {
+    console.log(req.body.username);
+    res.redirect('/');
 })
 
 app.use((req,res,next) => {
