7c78616822c1f68c4bae7c24337a7960bb56a012
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-11 12:58:21 +0530

    Contact completed

--- Express Intro/app.js
+++ Express Intro/app.js
@@ -1,27 +1,23 @@
 const http = require('http');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
+const path = require('path')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'Public')))
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In the next middleware!!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
-})
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
 
-app.use('/product',(req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use((req, res, next) => {
-    console.log('In the next middleware!!');
-    res.send('<h1>Welcome to Express!!</h1>')
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','error.html'))
 })
 
 app.listen(3000);
