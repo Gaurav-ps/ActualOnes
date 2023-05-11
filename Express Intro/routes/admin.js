e1c202612b40f5bb48cd41413c7032456bab9245
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-11 12:56:55 +0530

    Contact us completed route

--- Express Intro/routes/admin.js
+++ Express Intro/routes/admin.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
+const path = require('path')
+
+const rooDir = require('../util/path')
+
 const router = express.Router();
 
 router.get('/add-product',(req, res, next) => {
-    console.log('In the next middleware!!');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(rooDir,'views','add-product.html'))
 })
 
 router.post('/add-product',(req, res, next) => {
     console.log(req.body)
-    res.redirect('/shop');
+    res.redirect('/');
+})
+
+router.get('/contactus',(req,res,next) => {
+    res.sendFile(path.join(rooDir,'views','contact.html'))
+})
+
+router.get('/success',(req,res,next) => {
+    res.send('Form Successfully Filled!!')
 })
 
 module.exports = router;
