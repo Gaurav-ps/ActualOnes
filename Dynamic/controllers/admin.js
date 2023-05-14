049c83efb9375c3cf6e2ac91310b9641abb4bf6d
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-14 18:39:34 +0530

    Sequelize Project

--- Dynamic/controllers/admin.js
+++ Dynamic/controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const sequelize = require('../util/database')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -14,10 +15,13 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null,title, imageUrl, description, price);
-  product.save()
-  .then(() => {
-    res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
   })
+  .then(() => {})
   .catch(err => console.log(err))
   
 };
@@ -60,10 +64,10 @@ exports.postEditProduct = (req,res,next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows,fieldData]) => {
+  Product.findAll()
+  .then((products) => {
     res.render('admin/products', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
@@ -73,8 +77,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req,res,next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-  .then(() => {
+  Product.findById(prodId)
+  .then((product) => {
+    return product.destroy();
+  })
+  .then(()=> {
     res.redirect('/admin/products')
   })
   .catch(err => console.log(err))
