e3b77a841c35c788f467fb34cea05f15cae1e5db
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-16 17:21:09 +0530

    Expense backend base

---
+++ Dynamic/models/expenses.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database')
+
+const Expenses = sequelize.define('expenses',{
+    id : {
+        type : Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    amount:{
+        type: Sequelize.INTEGER, 
+    },
+    description: {
+        type: Sequelize.STRING,
+    }
+})
+
+module.exports = Expenses;
+
