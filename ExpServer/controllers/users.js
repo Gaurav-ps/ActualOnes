253f1a4b60308967acd6514654b29c09a7719cec
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-22 22:44:54 +0530

    Login backend

--- ExpServer/controllers/users.js
+++ ExpServer/controllers/users.js
@@ -1,13 +1,24 @@
-const Users = require('../models/users')
+const User = require('../models/users')
 
 const addUsers = async(req,res,next) => {
+    function stringValidate(string){
+        if(string === undefined || string.length === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     try{
         console.log(req.body)
         const name = req.body.Name
         const email = req.body.Email
-        const password = req.body.Password
-        const data = await Users.create({name: name, email: email, password: password})
-        res.status(201).json({userDetails: data})
+        const password = req.body.Password 
+        if(stringValidate(name) || stringValidate(email) || stringValidate(password)){
+            return res.status(400).json({err: 'Bad Parameters'})
+        }
+        await User.create({name:name, email:email, password:password})
+        res.status(201).json({message: 'User created successfully'})
     }
     catch(err){
         console.log(err)
@@ -17,17 +28,36 @@ const addUsers = async(req,res,next) => {
     }
 }
 
-const getUsers = async(req,res,next) => {
+const loginUsers = async(req,res,next) => {
     try{
-        const allUsers = await Users.findAll()
-        res.status(200).json({existingUsers: allUsers})
+        let flag = true;
+        const email = req.body.Email
+        const password = req.body.Password
+        const allUsers = await User.findAll();
+        const allUsersData = JSON.parse(JSON.stringify(allUsers))
+        for(let i=0; i<allUsersData.length; i++)
+        {
+            if(allUsersData[i].email === email && allUsersData[i].password === password){
+                res.status(200).json({message: 'Login Successful!!'})
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            res.status(404).json({message: "User doesn't exists"})
+        }
+
     }
     catch(err){
         console.log(err)
+        res.status(401).json({
+            error: 'Invalid Credentials'
+        })
     }
 }
 
 module.exports = {
     addUsers,
-    getUsers
+    loginUsers
 }
