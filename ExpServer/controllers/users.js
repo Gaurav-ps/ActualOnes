2aceba7c57ead4a4d4974a374462d603058d6a62
Gaurav-ps <gauravpshetty96@gmail.com>
2023-05-23 11:07:24 +0530

    Password encryption

--- ExpServer/controllers/users.js
+++ ExpServer/controllers/users.js
@@ -1,24 +1,29 @@
 const User = require('../models/users')
+const bcrypt = require('bcrypt')
+
+function stringValidate(string){
+    if(string === undefined || string.length === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 const addUsers = async(req,res,next) => {
-    function stringValidate(string){
-        if(string === undefined || string.length === 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
     try{
-        console.log(req.body)
         const name = req.body.Name
         const email = req.body.Email
         const password = req.body.Password 
         if(stringValidate(name) || stringValidate(email) || stringValidate(password)){
             return res.status(400).json({err: 'Bad Parameters'})
         }
-        await User.create({name:name, email:email, password:password})
-        res.status(201).json({message: 'User created successfully'})
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds,async(err,hash)=>{
+            console.log(err)
+            await User.create({name:name, email:email, password:hash})
+            res.status(201).json({message: 'User created successfully'})
+        })
     }
     catch(err){
         console.log(err)
@@ -30,29 +35,33 @@ const addUsers = async(req,res,next) => {
 
 const loginUsers = async(req,res,next) => {
     try{
-        let flag = true;
         const email = req.body.Email
         const password = req.body.Password
-        const allUsers = await User.findAll();
-        const allUsersData = JSON.parse(JSON.stringify(allUsers))
-        for(let i=0; i<allUsersData.length; i++)
-        {
-            if(allUsersData[i].email === email && allUsersData[i].password === password){
-                res.status(200).json({message: 'Login Successful!!'})
-                flag = false;
-                break;
-            }
+        if(stringValidate(email) || stringValidate(password)){
+            return res.status(400).json({message: 'Bad parameters',success:false})
         }
-        if(flag)
-        {
-            res.status(404).json({message: "User doesn't exists"})
+        const allUsers = await User.findAll({where: {email}})
+        if(allUsers.length > 0){
+            bcrypt.compare(password,allUsers[0].password,(err,result) => {
+                if(err){
+                    return res.status(500).json({success:false, message:'Error'})
+                }
+                if(result == true){
+                   return res.status(200).json({success: true, message:'Login successful'})
+                }
+                else{
+                    return res.status(400).json({success: false, message:'Incorrect password'})
+                }
+            })
         }
-
+        else{
+            return res.status(404).json({success: false, message:'User not found'})
+        }
     }
     catch(err){
         console.log(err)
-        res.status(401).json({
-            error: 'Invalid Credentials'
+        res.status(500).json({
+            message: err, success:false
         })
     }
 }
